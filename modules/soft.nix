b39a4ba6cb691d974c3268f1e2010577ba5225a6
Rail Aliiev <rail@mozilla.com>
2018-08-03 16:12:26 -0400

    Update

--- modules/soft.nix
+++ modules/soft.nix
@@ -2,6 +2,14 @@
 let
   firefoxEnv = pkgs.callPackage ../packages/nightly.nix { };
   unstable = import <nixos-unstable> {};
+  pypi2nix_upstream = pkgs.pypi2nix.overrideAttrs (old: {
+    src = pkgs.fetchFromGitHub {
+      owner = "garbas";
+      repo = "pypi2nix";
+      rev = "ec42eaab4b8f4e0cdc0c8045f924cef31ea01da1";
+      sha256 = "0zxwjvrn3sn25z0h1nqfzyz4kf1hnyz92699snzcfa2pi58bb969";
+    };
+  });
 in
 
 {
@@ -12,10 +20,7 @@ in
 
   nixpkgs.config.allowUnfree = true;
   programs.bash.enableCompletion = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  programs.gnupg.agent.enable = true;
 
   environment.systemPackages = with pkgs; [
     # crashplan-pro
@@ -26,7 +31,7 @@ in
     clementine
     curl
     file
-    firefox-nightly-bin
+    latest.firefox-nightly-bin
     fzf
     gitAndTools.gitFull
     gnupg
@@ -48,12 +53,15 @@ in
     netcat-gnu
     networkmanagerapplet
     nix-prefetch-scripts
+    p7zip
     pavucontrol
     pinentry
     pass
     polkit_gnome
+    pypi2nix_upstream
     pwgen
     rsync
+    unstable.pkgs.skopeo
     silver-searcher
     sshfs
     strace
