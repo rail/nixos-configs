f43c900c63a273ec362ab4d6959ab5616d8e0f4a
Rail Aliiev <rail@mozilla.com>
2018-08-21 12:10:59 -0400

    No more xautolock

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -35,46 +35,32 @@
       default = "i3";
       i3.enable = true;
     };
-    xautolock =  {
-      enable = true;
-      killer = null;
-      extraOptions = [ "-lockaftersleep" ];
-      locker = "${pkgs.i3lock-fancy}/bin/i3lock-fancy -f Hack -p -g";
-    };
   };
 
   services.redshift = {
     enable = true;
     provider = "geoclue2";
   };
-  programs.light.enable = true;
 
   # make GDM find other WMs
-  system.activationScripts.etcX11sessions = ''
-    echo "setting up /etc/X11/sessions..."
-    mkdir -p /etc/X11
-    [[ ! -L /etc/X11/sessions ]] || rm /etc/X11/sessions
-    ln -sf ${config.services.xserver.displayManager.session.desktops} /etc/X11/sessions
-  '';
+  # system.activationScripts.etcX11sessions = ''
+  #   echo "setting up /etc/X11/sessions..."
+  #   mkdir -p /etc/X11
+  #   [[ ! -L /etc/X11/sessions ]] || rm /etc/X11/sessions
+  #   ln -sf ${config.services.xserver.displayManager.session.desktops} /etc/X11/sessions
+  # '';
 
+  # TODO: make this work and remove from i3 config
   # systemd.user.services.lightlocker = {
   #   description = "Light locker";
   #   wantedBy = [ "graphical-session.target" ];
   #   partOf = [ "graphical-session.target" ];
   #   serviceConfig = {
-  #     ExecStart = "${pkgs.lightlocker}/bin/light-locker --lock-after-screensaver=10 --late-locking --idle-hint";
-  #     RestartSec = 3;
+  #     ExecStart = "${pkgs.lightlocker}/bin/light-locker --lock-after-screensaver=10 --late-locking --idle-hint --lock-on-suspend";
+  #     RestartSec = 15;
   #     Restart = "always";
-  #   };
-  # };
-  # systemd.user.services.xscreensaver = {
-  #   description = "XScreenSaver";
-  #   wantedBy = [ "graphical-session.target" ];
-  #   partOf = [ "graphical-session.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.xscreensaver}/bin/xscreensaver -no-splash";
-  #     RestartSec = 3;
-  #     Restart = "always";
+  #     Environment = "XDG_SESSION_PATH=/org/freedesktop/DisplayManager/Session0";
+  #     PassEnvironment = "DISPLAY";
   #   };
   # };
 
