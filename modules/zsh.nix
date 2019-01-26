659719b30e9485a95b8f589b20f0f23a9330d144
Rail Aliiev <rail@mozilla.com>
2019-01-26 00:25:33 -0500

    cleanup

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -9,10 +9,7 @@
       enable = true;
       plugins =
         [ "git"
-          "systemd"
           "pass"
-          "colorize"
-          "colored-man-pages"
           "virtualenvwrapper" 
         ];
       theme = "frisk";
@@ -25,31 +22,5 @@
       tmux = "tmux -2";
       mutt = "TERM=screen-256color mutt";
     };
-    interactiveShellInit = ''
-      setopt print_exit_value
-      unsetopt share_history
-      FIGNORE=".o:~"
-      LISTMAX=0
-      LOGCHECK=300
-      MAILCHECK=0
-      REPORTTIME=60
-
-      zstyle :omz:plugins:ssh-agent agent-forwarding on
-      zstyle :omz:plugins:ssh-agent lifetime 4h
-      zstyle ':vcs_info:*' enable git
-      setopt NOCLOBBER
-      setopt no_nomatch # when pattern matching fails, simply use the command as is
-
-      export BROWSER=firefox
-      export WORKON_HOME=~/.virtualenvs
-
-      function wp() {
-          cd ~/work/git/$1;
-      }
-      _wp() {
-          _files -W ~/work/git/
-      }
-      compdef _wp wp
-    '';
   };
 }
