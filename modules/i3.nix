fd9d039bfe79c3a3070f85680fb077c6bffcb012
Rail Aliiev <rail@mozilla.com>
2019-12-06 15:50:43 -0500

    No more compton, slowing down CPU

--- modules/i3.nix
+++ modules/i3.nix
@@ -12,13 +12,4 @@
       i3lock-color
     ];
   };
-
-  services.compton = {
-    enable = false;
-    fade = true;
-    fadeDelta = 2;
-    shadow = true;
-    shadowExclude = [ "window_type *= 'menu'" "window_type *= 'dock'" "window_type *= 'dnd'"];
-  };
-
 }
