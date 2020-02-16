aad6a2ae0e5f20f98bc2b5ff5f2d59db517b698e
Rail Aliiev <rail@mozilla.com>
2020-02-15 22:47:32 -0500

    more changes

--- machines/x1-gen5-btrfs.sh
+++ machines/x1-gen5-btrfs.sh
@@ -52,8 +52,13 @@ mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=zstd,space_cache /dev/
 btrfs subvolume create /mnt/nixos
 umount /mnt
 mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=zstd,space_cache,subvol=nixos /dev/mapper/root /mnt/
+
 btrfs subvolume create /mnt/home
+mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=zstd,space_cache,subvol=home /dev/mapper/root /mnt/home
+
+mkdir -p /mnt/var/lib
 btrfs subvolume create /mnt/var/lib/docker
+mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=zstd,space_cache,subvol=var/lib/docker /dev/mapper/root /mnt/var/lib/docker
 
 mkdir -p /mnt/boot
 mount /dev/disk/by-partlabel/boot /mnt/boot
